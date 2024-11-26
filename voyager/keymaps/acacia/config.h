79e156283b638fc046fcfa7eaf9c0f69cb03733f
Jens Peter Secher <jpsecher@gmail.com>
2024-11-26 20:10:10 +0100

    Acacia transferred to Voyager
    clone https://github.com/zsa/qmk_firmware/
    cp to keyboards/zsa/voyager/keymaps/acacia and run
    make zsa/voyager:acacia:flash

---
+++ voyager/keymaps/acacia/config.h
@@ -0,0 +1,28 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 225
+
+// Cancel one-shot after one and a half second
+#undef ONESHOT_TIMEOUT
+#define ONESHOT_TIMEOUT 1500
+
+#define PERMISSIVE_HOLD
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+// Smart caps lock cancels after 10 seconds.
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define CAPS_WORD_IDLE_TIMEOUT 10000
+
+#undef RGB_MATRIX_TIMEOUT
+#define RGB_MATRIX_TIMEOUT 300000
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
