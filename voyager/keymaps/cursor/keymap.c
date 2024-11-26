19d96f4818b3fb6bb16b762f019331b2ee540696
Jens Peter Secher <jpsecher@gmail.com>
2024-11-26 22:58:27 +0100

    move 0

--- voyager/keymaps/cursor/keymap.c
+++ voyager/keymaps/cursor/keymap.c
@@ -93,8 +93,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_TOG,  _______,  _______,  _______,  _______,  _______,                         _______,  _______,  _______,  _______,  _______,  QK_BOOT,        
     _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_QUES,                         KC_MINUS, KC_7,     KC_8,     KC_9,     _______,  _______, 
     _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_SCLN,                         KC_COLN,  KC_4,     KC_5,     KC_6,     KC_PLUS,  KC_EQUAL,       
-    _______,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,                         KC_COMM,  KC_1,     KC_2,     KC_3,     KC_DOT,   _______, 
-                                                      _______,  _______,     _______,  KC_0
+    _______,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,                         KC_0,     KC_1,     KC_2,     KC_3,     KC_DOT,   _______, 
+                                                      _______,  _______,     _______,  KC_COMM
   ),
   [_SYMBOLS] = LAYOUT_voyager(
     KC_MNXT,  _______,  _______,  _______,  _______,  _______,                         _______,  _______,  _______,  _______,  _______,  KC_MPRV,
---
+++ voyager/keymaps/cursor/rules.mk
@@ -0,0 +1,7 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
